8a56e1803aa72fb392ef2295e1b16699fe4ae921
caarlosdamian <dapecarlos@gmail.com>
2023-01-15 11:20:51 -0600

    feat(deps): added new depencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./common/app/App";
+import  "./common/app/App.scss";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
